67efb615f242fa75503ea516df0cd5f4f9dc90c2
Lynn tseng <lynnpython1222@gmail.com>
2021-02-24 09:48:17 +0800

    input_practice

---
+++ input_practice.py
@@ -0,0 +1,14 @@
+price = 10
+pen = 0.38
+rain = True
+name = 'Lynn'
+print(price)
+print(pen)
+print(rain)
+print(name)
+print(price, pen, rain, name)
+
+name = input('enter your name:')
+weight = input('enter your weight')
+high = input('enter your 身高')
+print('Hi', name, '你的身高是', weight, '你的體重是', high)
